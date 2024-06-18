5a6c12ec3d37b809a3532ae178af509abd852457
Pedro Rocha <96178193+pedro-rocha-maersk@users.noreply.github.com>
2024-06-18 09:50:27 +0100

    Fix labelary

--- src/App.js
+++ src/App.js
@@ -184,7 +184,7 @@ function App() {
       console.log("label", label);
 
       const labelImageResponse = await fetch(
-        `http://api.labelary.com/v1/printers/8dpmm/labels/4x6/0/`,
+        `https://api.labelary.com/v1/printers/8dpmm/labels/4x6/0/`,
         {
           method: "POST",
           headers: {
